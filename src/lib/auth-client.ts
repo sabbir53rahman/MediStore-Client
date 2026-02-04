897dd6b3724d65b77d01cb9b9fb28922749a0686
sabbir53rahman <sabbir53rahman@gmail.com>
2026-02-04 07:02:53 +0600

    only add baseUrl on the authclient

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "https://medi-store-server-rose.vercel.app",
+  baseURL: "http://localhost:5000",
 });
